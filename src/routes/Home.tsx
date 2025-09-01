edfbef8f70e4dfafb5bc9887f21bed3c5bf603c3
tiago-forward <devtiagoforward@gmail.com>
2025-09-01 19:40:14 -0300

    chore: add midias in home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import MenuLink from "../components/ui/MenuLink";
 import UpDetailMain from "../assets/details/up-detail-main.png";
 import LowenDetailMain from "../assets/details/lowen-detail-main.png";
+import { Github, Instagram, Linkedin } from "lucide-react";
 
 type AllowedTo =
   | "/profile"
@@ -23,7 +24,7 @@ export default function Home() {
   const [selected, setSelected] = React.useState(items[0].to);
 
   return (
-    <section className="flex min-h-[70dvh] gap-8 flex-col items-center justify-center text-center px-6">
+    <section className="flex min-h-[70dvh] gap-2 lg:gap-4 flex-col items-center justify-center text-center px-6">
       <div className="">
         <img src={UpDetailMain} alt="up detail skills" className="m-auto" />
         <h1
@@ -50,6 +51,23 @@ export default function Home() {
             {it.label}
           </MenuLink>
         ))}
+      </div>
+      <div className="flex justify-between items-end w-full max-w-2xl">
+        <div className="flex items-center gap-1">
+          <a href="https://github.com/tiago-forward" target="_blank">
+            <Github size={14} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tiago-lacerda-devfrontend"
+            target="_blank"
+          >
+            <Linkedin size={14} color="#0073B2" />
+          </a>
+          <a href="https://www.instagram.com/tiago_forward/" target="_blank">
+            <Instagram size={14} color="#FC02AF" />
+          </a>
+        </div>
+        <div></div>
       </div>
     </section>
   );
